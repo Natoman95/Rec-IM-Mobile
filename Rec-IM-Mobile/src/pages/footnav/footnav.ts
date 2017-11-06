e0febb58873e5296e5f7a18f5b7b2dc70d60c567
Nathan Gray <nathan.gray@gordon.edu>
2017-11-05 21:24:56 -0500

    Adding Alert dialogue for calling GoPo

--- Rec-IM-Mobile/src/pages/footnav/footnav.ts
+++ Rec-IM-Mobile/src/pages/footnav/footnav.ts
@@ -1,6 +1,6 @@
 import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'foot-nav',
@@ -8,7 +8,7 @@ import { NavController } from 'ionic-angular';
 })
 export class FootNav {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
@@ -18,5 +18,26 @@ export class FootNav {
 
   home() {
     this.navCtrl.push(HomePage);
+  }
+
+  callGoPoAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Call Gordon Police',
+      message: 'Call Gordon Police Emergency Number?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Call',
+          handler: () => {
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
