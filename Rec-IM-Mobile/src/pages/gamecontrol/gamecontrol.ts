f702edc0a654de62d707c6f9f0c276bd3fa7cd6a
Nathan Gray <nathan.gray@gordon.edu>
2017-11-06 12:14:01 -0500

    Scoring and timeouts to gamecontrol page

--- Rec-IM-Mobile/src/pages/gamecontrol/gamecontrol.ts
+++ Rec-IM-Mobile/src/pages/gamecontrol/gamecontrol.ts
@@ -9,11 +9,19 @@ import { NavController } from 'ionic-angular';
 
 export class GameControlPage {
 
+  private teamOneScore: number = 0;
+  private teamTwoScore: number = 0;
+
+  private teamOneTimeouts: number = 3;
+  private teamTwoTimeouts: number = 3;
+
   constructor(public navCtrl: NavController, public timerSvc: GameTimerService) { }
 
   ngOnInit() {
     this.timerSvc = new GameTimerService();
   }
+
+  // Timer methods
 
   startTimer() {
     this.timerSvc.startTimer();
@@ -33,5 +41,65 @@ export class GameControlPage {
 
   getHours() {
     return this.timerSvc.getHours();
+  }
+
+  // Score methods
+
+  getTeamOneScore() {
+    return this.teamOneScore;
+  }
+
+  getTeamTwoScore() {
+    return this.teamTwoScore;
+  }
+
+  addTeamOneScore() {
+    this.teamOneScore = ++this.teamOneScore;
+  }
+
+  subTeamOneScore() {
+    if (this.teamOneScore > 0) {
+      this.teamOneScore = --this.teamOneScore;
+    }
+  }
+
+  addTeamTwoScore() {
+    this.teamTwoScore = ++this.teamTwoScore;
+  }
+
+  subTeamTwoScore() {
+    if (this.teamTwoScore > 0) {
+      this.teamTwoScore = --this.teamTwoScore;
+    }
+  }
+
+  // Timeout methods
+
+  getTeamOneTimeouts() {
+    return this.teamOneTimeouts;
+  }
+
+  getTeamTwoTimeouts() {
+    return this.teamTwoTimeouts;
+  }
+
+  addTeamOneTimeouts() {
+    this.teamOneTimeouts = ++this.teamOneTimeouts;
+  }
+
+  subTeamOneTimeouts() {
+    if (this.teamOneTimeouts > 0) {
+      this.teamOneTimeouts = --this.teamOneTimeouts;
+    }
+  }
+
+  addTeamTwoTimeouts() {
+    this.teamTwoTimeouts = ++this.teamTwoTimeouts;
+  }
+
+  subTeamTwoTimeouts() {
+    if (this.teamTwoTimeouts > 0) {
+      this.teamTwoTimeouts = --this.teamTwoTimeouts;
+    }
   }
 }
